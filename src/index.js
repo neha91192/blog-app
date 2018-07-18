9c9de4a0ffa817236746013feef505a0e0831197
Neha <shukla.ne@husky.neu.edu>
2018-07-18 14:50:05 -0400

    removed react files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import BlogApp from "./BlogApp";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<BlogApp />, document.getElementById('root'));
+
